d6f1dc05c32c5963fb8d19483aa18d6ba0cd6873
OwethuSotomela <owethusotomela@gmail.com>
2022-02-21 15:07:33 +0200

    Random number function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 var selectNum = document.querySelector(".selectNumber");
+var selectedNum = document.querySelector(".selectedNum");
+var feedbacElem = document.querySelector(".feedback");
 
+let anyNumber = document.getElementById(".selectNum").value;
+console.log(anyNumber);
 
-function guessGame() {
+function randomNum() {
     let randomNumber = Math.ceil((Math.random() * 100))
+    if (randomNumber >= anyNumber) {
+        return "Your guess is too high!"
+    }
+    if (randomNumber <= anyNumber) {
+        return "Your guess is too low!"
+    }
+    return 'Correct, the secret number is ${number}'
 }
 
-function rockPlayer() {
-    playerChoice = document.querySelector(".playerRock").value;
-   
-}
+
+
+
