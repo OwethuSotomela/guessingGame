0342d5890acca4ac09549b07eb1ced18dc7bd9db
OwethuSotomela <owethusotomela@gmail.com>
2022-02-21 12:19:27 +0200

    Skeleton

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var selectNum = document.querySelector(".selectNumber");
+
+
+function guessGame() {
+    let randomNumber = Math.ceil((Math.random() * 100))
+}
+
+function rockPlayer() {
+    playerChoice = document.querySelector(".playerRock").value;
+   
+}
